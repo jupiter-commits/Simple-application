67fd8ae54bb8e3bb4faae2349aec2eb8bcb28a48
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-02-08 22:02:24 +0200

    Upload second version

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+import (
+"fmt"
+"os"
+"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func printerFunc(writer http.ResponseWriter, request *http.Request){
+	message := `{"text":"Hello world"}`
+	fmt.Fprintln(writer, message)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, `<h1 style="background-color: yellow;text-transform: capitalize;">%s</h1><div>%s</div>`, p.Title, p.Body)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".html"
+	body, err := os.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main(){
+	const port = ":80"
+
+	fmt.Println("Starting go app");
+
+	http.HandleFunc("/view/", viewHandler)
+	
+	http.HandleFunc("/print", printerFunc)
+	http.ListenAndServe(port, nil);
+}
